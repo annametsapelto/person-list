a8d2b75c24a6c54ea02c89680a97565856948312
annametsapelto <anna.metsapelto@gmail.com>
2020-06-25 19:44:15 +0300

    Format Person boxes.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
+import './Person.css'
 
 function Person(props) {
 
-  const StyledDiv = styled.div`
-    width: 300px;
-    border: 1px solid #333
-    margin: 10px
-    text-align: center
-    padding: 5px`
   return (
-    <StyledDiv><p onClick={props.click}>Name: {props.name} Age: {props.age}</p>
-  <input type="text" onChange={props.change} value={props.name}/>
-  </StyledDiv>
+    <div className="person"><p onClick={props.click}>Name: {props.name} Age: {props.age}</p>
+      <input type="text" onChange={props.change} value={props.name}/>
+    </div>
   
   )
 }
